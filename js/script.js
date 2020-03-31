5cf7daa41d79f2be4723048d04b5b981de6e8ee5
Alan <alan_comsytem10@outlook.es>
2020-03-31 17:53:22 -0600

    Identacion de todos los archivos

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-/*FECHAS RESERVA*/ 
+/*FECHAS RESERVA*/
 $('.datepicker.entrada').datepicker({
-    startDate:'0d',
+    startDate: '0d',
     format: 'dd-mm-yyyy',
-    todayHighlight:true
+    todayHighlight: true
 });
-$('.datepicker.entrada').change(function(){
+$('.datepicker.entrada').change(function () {
 
     var fechaEntrada = $(this).val();
 
@@ -15,23 +15,23 @@ $('.datepicker.entrada').change(function(){
     });
 })
 
-/*BOTON RESERVA*/ 
-$(".mostrarBloqueReservas").click(function(){
+/*BOTON RESERVA*/
+$(".mostrarBloqueReservas").click(function () {
 
     $(".formReservas").slideToggle("fast");
     $(".menu").slideUp('fast');
 
-    if($(".mostrarBloqueReservas").attr("modo") == "abajo"){
+    if ($(".mostrarBloqueReservas").attr("modo") == "abajo") {
 
         $(".mostrarBloqueReservas").attr("modo", "arriba");
 
         $(".reservaFlecha").removeClass("fa-caret-square-up");
         $(".reservaFlecha").addClass("fa-caret-square-down");
 
-    }else{
+    } else {
 
-    $(".mostrarBloqueReservas").attr("modo", "abajo");
-    
+        $(".mostrarBloqueReservas").attr("modo", "abajo");
+
         $(".reservaFlecha").removeClass("fa-caret-square-down");
         $(".reservaFlecha").addClass("fa-caret-square-up");
     }
@@ -40,33 +40,33 @@ $(".mostrarBloqueReservas").click(function(){
 })
 
 /*ANIMACIONES DEL SCROLL*/
-$(window).scroll(function(){
+$(window).scroll(function () {
 
     var posY = window.pageYOffset;
-    
-    if(window.matchMedia("min-width:760px").matches){
 
-        if(posY > 50){
+    if (window.matchMedia("min-width:760px").matches) {
+
+        if (posY > 50) {
 
             $(".formReservas").slideUp("fast");
             $(".mostrarBloqueReservas").attr("modo", "arriba");
             $(".reservaFlecha").removeClass("fa-caret-square-up");
             $(".reservaFlecha").addClass("fa-caret-square-down");
 
-        }else{
+        } else {
 
             $(".formReservas").slideDown("fast");
             $(".mostrarBloqueReservas").attr("modo", "abajo");
             $(".reservaFlecha").removeClass("fa-caret-square-down");
             $(".reservaFlecha").addClass("fa-caret-square-up");
-        }       
+        }
     }
 
     posicionBloqueReservas();
 })
 
 /*BOTONES DE IDIOMAS*/
-$(".idiomaIngles").click(function(){
+$(".idiomaIngles").click(function () {
 
     $(this).removeClass("bg-white")
     $(this).removeClass("text-dark")
@@ -74,7 +74,7 @@ $(".idiomaIngles").click(function(){
     $(this).addClass("bg-secondary")
     $(this).addClass("text-white")
 
-    
+
     $(".idiomaEspañol").removeClass("bg-secondary")
     $(".idiomaEspañol").removeClass("text-white")
 
@@ -82,14 +82,14 @@ $(".idiomaIngles").click(function(){
     $(".idiomaEspañol").addClass("text-dark")
 })
 
-$(".idiomaEspañol").click(function(){
+$(".idiomaEspañol").click(function () {
     $(this).removeClass("bg-white")
     $(this).removeClass("text-dark")
 
     $(this).addClass("bg-secondary")
     $(this).addClass("text-white")
 
-    
+
     $(".idiomaIngles").removeClass("bg-secondary")
     $(".idiomaIngles").removeClass("text-white")
 
@@ -98,51 +98,53 @@ $(".idiomaEspañol").click(function(){
 })
 
 /*BOTON MENU*/
-if(window.matchMedia("(max-width:768px)").matches){
-    
-    $(".botonMenu").click(function(){
+if (window.matchMedia("(max-width:768px)").matches) {
+
+    $(".botonMenu").click(function () {
 
         $(".menuMovil").slideToggle('fast');
-        $(".menuMovil").css({"top":$("header").height()})
+        $(".menuMovil").css({
+            "top": $("header").height()
+        })
 
     })
 
-    $(".menuMovil ul li a").click(function(e){
+    $(".menuMovil ul li a").click(function (e) {
         $(".menuMovil").slideToggle('fast');
         e.preventDefault();
-    
+
         var vinculo = $(this).attr("href");
-        
+
         $("html, body").animate({
             scrollTop: $(vinculo).offset().top - 65
         }, 1000, "easeInOutBack")
-        
+
     })
 
-    }else{
+} else {
 
-    $(".botonMenu").click(function(){
-        
+    $(".botonMenu").click(function () {
+
         $(".menu").slideToggle('fast');
         $(".formReservas").slideUp('fast');
     })
 
-    $(".menu ul li a").click(function(e){
+    $(".menu ul li a").click(function (e) {
         $(".menu").slideToggle('fast');
         e.preventDefault();
-    
+
         var vinculo = $(this).attr("href");
-        
+
         $("html, body").animate({
             scrollTop: $(vinculo).offset().top - 75
         }, 1000, "easeInOutBack")
-        
+
     })
 }
 
 /*SCROLL UP*/
 $.scrollUp({
-    scrollText:"",
+    scrollText: "",
     scrollSpeed: 2000,
     easingType: "easeOutQuint"
 })
@@ -150,39 +152,47 @@ $.scrollUp({
 
 /*SLIDE DEL BANNER*/
 $(".fade-slider").jdSlider({
-  isSliding: false,
-  isAuto: true,
-  isLoop: true,
-  isDreg: false,
-  interval: 7000,
-  isCursosr: false,
-  speed: 3000  
+    isSliding: false,
+    isAuto: true,
+    isLoop: true,
+    isDreg: false,
+    interval: 7000,
+    isCursosr: false,
+    speed: 3000
 })
-$(".verMas").click(function(){
+$(".verMas").click(function () {
 
-   var vinculo = $(this).attr("vinculo");
+    var vinculo = $(this).attr("vinculo");
 
-   $("html, body").animate({
-    scrollTop: $(vinculo).offset().top - 75
-   }, 1000, "easeInOutBack")
+    $("html, body").animate({
+        scrollTop: $(vinculo).offset().top - 75
+    }, 1000, "easeInOutBack")
 
 })
 
-$(".banner .fade-slider").css({"margin-top":$("header").height()})
+$(".banner .fade-slider").css({
+    "margin-top": $("header").height()
+})
 
 /*INTERACCION PLANES*/
-$(".planes .grid-item").mouseover(function(){
+$(".planes .grid-item").mouseover(function () {
 
-    $(this).children("figure").css({"height":"25%", "transition":".5s all"})
+    $(this).children("figure").css({
+        "height": "25%",
+        "transition": ".5s all"
+    })
 
     $(".tituloPlan").html($(this).children("figure").children("h1").html());
     $(".descripcionPlan").html($(this).children("figure").children("h1").attr("descripcion"));
 
 })
 
-$(".planes .grid-item").mouseout(function(){
+$(".planes .grid-item").mouseout(function () {
 
-    $(this).children("figure").css({"height":"100%", "transition":".5s all"})
+    $(this).children("figure").css({
+        "height": "100%",
+        "transition": ".5s all"
+    })
 
     $(".tituloPlan").html($(".tituloPlan").attr("tituloPlan"));
     $(".descripcionPlan").html($(".descripcionPlan").attr("descripcionPlan"));
@@ -192,19 +202,19 @@ $(".planes .grid-item").mouseout(function(){
 /*PLANES MOVIL*/
 $('.planesMovil').jdSlider({
     wrap: '.slide-inner',
-    slideToScroll:3,
+    slideToScroll: 3,
     slideShow: 3,
     isLoop: false
 })
 
-$(".planesMovil li").click(function(){
+$(".planesMovil li").click(function () {
 
     $(".modal-title").html($(this).children("a").children("h6").html());
     $(".modal-body img").attr("src", $(this).children("a").children("img").attr("src"));
     $(".modal-body p").html($(this).children("a").attr("descripcion"));
 })
 
-$(".planes .grid-item").click(function(){
+$(".planes .grid-item").click(function () {
 
     $(".modal-title").html($(this).children("figure").children("h1").html());
     $(".modal-body img").attr("src", $(this).children("img").attr("src"));
@@ -217,35 +227,43 @@ $('.slidePueblo').jdSlider();
 /*RESTAURANTE*/
 $(".restaurante .carta div").hide();
 
-if(window.matchMedia("(max-width:768px)").matches){
+if (window.matchMedia("(max-width:768px)").matches) {
 
-    $(".restaurante .verCarta").click(function(){
-
-        $(".restaurante .bloqueRestaurant").slideToggle("fast")
-        $(".restaurante .carta div").slideToggle("fast");
-        $(".restaurante .carta div").css({"background":"rgba(0,0,0,0.7)"})
-        $(".contactenos").css({"margin-top":"0px"})
-    })
-
-    $(".restaurante .volverCarta").click(function(){
+    $(".restaurante .verCarta").click(function () {
 
         $(".restaurante .bloqueRestaurant").slideToggle("fast")
         $(".restaurante .carta div").slideToggle("fast");
-        $(".contactenos").css({"margin-top":"-80px"})
+        $(".restaurante .carta div").css({
+            "background": "rgba(0,0,0,0.7)"
+        })
+        $(".contactenos").css({
+            "margin-top": "0px"
+        })
     })
 
-}else{
+    $(".restaurante .volverCarta").click(function () {
 
-    $(".restaurante .verCarta").click(function(){
+        $(".restaurante .bloqueRestaurant").slideToggle("fast")
+        $(".restaurante .carta div").slideToggle("fast");
+        $(".contactenos").css({
+            "margin-top": "-80px"
+        })
+    })
+
+} else {
+
+    $(".restaurante .verCarta").click(function () {
 
         $(".restaurante .carta div").slideToggle("fast")
-        $(".restaurante .carta div").css({"background":"rgba(0,0,0,0.7)"})
+        $(".restaurante .carta div").css({
+            "background": "rgba(0,0,0,0.7)"
+        })
     })
 }
 
 /*SLIDE HABITACIONES*/
 $('.slideHabitaciones').jdSlider({
-    isSliding:true,
+    isSliding: true,
     isAuto: true,
     isLoop: true,
     isDreg: true,
@@ -262,81 +280,102 @@ $("#myPano").pano({
 });
 
 /*VISUALIZAR MULTIMMEDIA HABITACION*/
-$(".colIzqHabitaciones button").click(function(){
+$(".colIzqHabitaciones button").click(function () {
 
-	var vista = $(this).attr("vista");
+    var vista = $(this).attr("vista");
 
-	if(vista == "fotos"){
+    if (vista == "fotos") {
 
-		$(".slideHabitaciones").removeClass("d-none");
-		$(".slideHabitaciones").addClass("d-block");
+        $(".slideHabitaciones").removeClass("d-none");
+        $(".slideHabitaciones").addClass("d-block");
 
-		$(".videoHabitaciones").addClass("d-none");
-		$(".videoHabitaciones").removeClass("d-block");
+        $(".videoHabitaciones").addClass("d-none");
+        $(".videoHabitaciones").removeClass("d-block");
 
-		$(".360Habitaciones").addClass("d-none");
-		$(".360Habitaciones").removeClass("d-block");
-	}
+        $(".360Habitaciones").addClass("d-none");
+        $(".360Habitaciones").removeClass("d-block");
+    }
 
-	if(vista == "video"){
+    if (vista == "video") {
 
-		$(".videoHabitaciones").removeClass("d-none");
-		$(".videoHabitaciones").addClass("d-block");
+        $(".videoHabitaciones").removeClass("d-none");
+        $(".videoHabitaciones").addClass("d-block");
 
-		$(".slideHabitaciones").addClass("d-none");
-		$(".slideHabitaciones").removeClass("d-block");
+        $(".slideHabitaciones").addClass("d-none");
+        $(".slideHabitaciones").removeClass("d-block");
 
-		$(".360Habitaciones").addClass("d-none");
-		$(".360Habitaciones").removeClass("d-block");
-	}
+        $(".360Habitaciones").addClass("d-none");
+        $(".360Habitaciones").removeClass("d-block");
+    }
 
-	if(vista == "360"){
+    if (vista == "360") {
 
-		$(".360Habitaciones").removeClass("d-none");
-		$(".360Habitaciones").addClass("d-block");
+        $(".360Habitaciones").removeClass("d-none");
+        $(".360Habitaciones").addClass("d-block");
 
-		$(".slideHabitaciones").addClass("d-none");
-		$(".slideHabitaciones").removeClass("d-block");
+        $(".slideHabitaciones").addClass("d-none");
+        $(".slideHabitaciones").removeClass("d-block");
 
-		$(".videoHabitaciones").addClass("d-none");
-		$(".videoHabitaciones").removeClass("d-block");
-	}
+        $(".videoHabitaciones").addClass("d-none");
+        $(".videoHabitaciones").removeClass("d-block");
+    }
 
 })
 
 /*POSICION BLOQUE RESERVAS*/
-function posicionBloqueReservas(){
-    if(window.matchMedia("(min-width:769px)").mathes){
+function posicionBloqueReservas() {
+    if (window.matchMedia("(min-width:769px)").mathes) {
 
-        if($(".mostrarBloqueReservas").attr("modo")=="abajo"){
+        if ($(".mostrarBloqueReservas").attr("modo") == "abajo") {
 
-            $(".colDerHabitaciones").css({"margin-top":"100px"})
+            $(".colDerHabitaciones").css({
+                "margin-top": "100px"
+            })
 
         }
 
-        if($(".mostrarBloqueReservas").attr("modo")=="arriba"){
+        if ($(".mostrarBloqueReservas").attr("modo") == "arriba") {
 
-            $(".colDerHabitaciones").css({"margin-top":"20px"})
+            $(".colDerHabitaciones").css({
+                "margin-top": "20px"
+            })
         }
-    
-    }else{
 
-        $(".colDerHabitaciones").css({"margin-top":"20px"})
+    } else {
+
+        $(".colDerHabitaciones").css({
+            "margin-top": "20px"
+        })
     }
 }
 
 posicionBloqueReservas();
 
-if(window.matchMedia("(max-width:768px)").matches){
+if (window.matchMedia("(max-width:768px)").matches) {
 
-    $(".infoHabitacion .colIzqHabitaciones").css({"margin-top":$("header").height()})
+    $(".infoHabitacion .colIzqHabitaciones").css({
+        "margin-top": $("header").height()
+    })
 }
 
 /*CALENDARIO*/
 $('#calendar').fullCalendar({
-    header:{
-        left:'prev',
-        center:'title',
-        right:'next',
+    header: {
+        left: 'prev',
+        center: 'title',
+        right: 'next',
     },
+    events: [{
+            start: '2020-03-12',
+            end: '2020-03-15',
+            rendering: 'background',
+            color: '#705e49'
+        },
+        {
+            start: '2020-03-12',
+            end: '2020-03-24',
+            rendering: 'background',
+            color: '#ffcc29'
+        }
+    ]
 });
